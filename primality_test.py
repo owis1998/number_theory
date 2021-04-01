6546f5aa09a9fcb08a9aed0205f668cdebb21f4f
owis1998 <74670290+owis1998@users.noreply.github.com>
2021-04-01 22:07:26 +0300

    Update primality_test.py

--- primality_test.py
+++ primality_test.py
@@ -1,6 +1,9 @@
 def trial_division(n):
-	for i in range(2, int(n ** 0.5)):
-		if n % i == 0:
+	if n < 2:
+		return False
+
+	for i in range(2, int(n ** 0.5) + 2):
+		if n % i == 0 and i != n:
 			return False
-	else
+	else:
 		return True
